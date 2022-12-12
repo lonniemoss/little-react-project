ca80a6dc58c88758c3164550cf71fbf1dfebf571
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2022-12-12 09:27:07 -0500

    react

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import {Component} from 'react';
+
+class Message extends Component {
+    render(){
+        return (
+            <div>
+                <h1>this is a class Component</h1>
+            </div>
+        )
+    }
+}
+export default Message;
